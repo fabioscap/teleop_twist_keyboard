cac7b8adf55e2a95f3ac1d0cd1b2402407e93be9
Fabio Scaparro <fabiovt_99@hotmail.it>
2023-03-08 17:57:22 +0000

    control reference cartesian position with keyboard

--- teleop_twist_keyboard.py
+++ teleop_twist_keyboard.py
@@ -7,8 +7,15 @@ import threading
 import roslib; roslib.load_manifest('teleop_twist_keyboard')
 import rospy
 
-from geometry_msgs.msg import Twist
-from geometry_msgs.msg import TwistStamped
+# we control /equilibrium_pose, which is a pose
+# not a velocity
+# from geometry_msgs.msg import Twist
+# from geometry_msgs.msg import TwistStamped
+
+from geometry_msgs.msg import Pose,PoseStamped,Quaternion
+
+
+import tf2_ros, tf.transformations as trans
 
 import sys
 from select import select
@@ -19,75 +26,99 @@ else:
     import termios
     import tty
 
+PoseMsg = Pose
 
-TwistMsg = Twist
+BASEFRAME = "panda_link0"
+EEFRAME   = "panda_EE"
 
 msg = """
 Reading from the keyboard  and Publishing to Twist!
 ---------------------------
 Moving around:
-   u    i    o
-   j    k    l
-   m    ,    .
+   X +/- : i/k
+   Y +/- : j/l
+   Z +/- : t/b
 
-For Holonomic mode (strafing), hold down the shift key:
----------------------------
-   U    I    O
-   J    K    L
-   M    <    >
+   Rx +/- : c/v
+   Ry +/- : n/m
+   Rz +/- : u/o
 
-t : up (+z)
-b : down (-z)
+q/z : increase/decrease linear offset by 10%
+w/x : increase/decrease angular offset by 10%
 
-anything else : stop
-
-q/z : increase/decrease max speeds by 10%
-w/x : increase/decrease only linear speed by 10%
-e/c : increase/decrease only angular speed by 10%
-
-CTRL-C to quit
+CTRL-C to quit (does not work)
 """
 
 moveBindings = {
-        'i':(1,0,0,0),
-        'o':(1,0,0,-1),
-        'j':(0,0,0,1),
-        'l':(0,0,0,-1),
-        'u':(1,0,0,1),
-        ',':(-1,0,0,0),
-        '.':(-1,0,0,1),
-        'm':(-1,0,0,-1),
-        'O':(1,-1,0,0),
-        'I':(1,0,0,0),
-        'J':(0,1,0,0),
-        'L':(0,-1,0,0),
-        'U':(1,1,0,0),
-        '<':(-1,0,0,0),
-        '>':(-1,-1,0,0),
-        'M':(-1,1,0,0),
-        't':(0,0,1,0),
-        'b':(0,0,-1,0),
+        # move along world x axis
+        'i':(1,0,0,0,0,0),
+        'k':(-1,0,0,0,0,0),
+        # move along world y axis
+        'j':(0,1,0,0,0,0),
+        'l':(0,-1,0,0,0,0),
+        # move along world z axis
+        't':(0,0,1,0,0,0),
+        'b':(0,0,-1,0,0,0),
+        # rotate along EE x axis
+        'c':(0,0,0,1,0,0),
+        'v':(0,0,0,-1,0,0),
+        # rotate along EE y axis
+        'n':(0,0,0,0,1,0),
+        'm':(0,0,0,0,-1,0),
+        # rotate along EE z axis
+        'u':(0,0,0,0,0,1),
+        'o':(0,0,0,0,0,-1),
     }
 
-speedBindings={
-        'q':(1.1,1.1),
-        'z':(.9,.9),
-        'w':(1.1,1),
-        'x':(.9,1),
-        'e':(1,1.1),
-        'c':(1,.9),
+offsetBindings={
+        'q':(1.1,1),
+        'z':(.9,1),
+        'w':(1,1.1),
+        'x':(1,.9),
     }
 
 class PublishThread(threading.Thread):
     def __init__(self, rate):
         super(PublishThread, self).__init__()
-        self.publisher = rospy.Publisher('cmd_vel', TwistMsg, queue_size = 1)
-        self.x = 0.0
-        self.y = 0.0
-        self.z = 0.0
-        self.th = 0.0
-        self.speed = 0.0
-        self.turn = 0.0
+
+        # get the topic
+        self.topic = rospy.get_param("out_topic", 
+                     default="/cartesian_impedance_example_controller/equilibrium_pose")
+
+        self.publisher = rospy.Publisher(self.topic, 
+                                         PoseMsg, 
+                                         queue_size = 1, # discard all but the last msg
+                                         )
+        
+        # get the starting pose
+        # http://wiki.ros.org/tf2/Tutorials/Writing%20a%20tf2%20listener%20%28Python%29
+        tfBuffer = tf2_ros.Buffer()
+        listener = tf2_ros.TransformListener(tfBuffer)
+        
+        msg = tfBuffer.lookup_transform(BASEFRAME, EEFRAME, 
+                                        rospy.Time(0), 
+                                        # TODO 
+                                        # timeout less than 10 causes ExtrapolationError
+                                        timeout=rospy.Duration(10)) 
+
+        pose = msg.transform
+        print(pose)
+        # pose.translation ...
+        # pose.rotation ...
+
+        self.x = pose.translation.x 
+        self.y = pose.translation.y
+        self.z = pose.translation.z
+        self.Q = [pose.rotation.x, 
+                  pose.rotation.y, 
+                  pose.rotation.z, 
+                  pose.rotation.w]
+        
+        
+
+
+        self.linear_offset = 0.0
+        self.angular_offset = 0.0
         self.condition = threading.Condition()
         self.done = False
 
@@ -111,60 +142,63 @@ class PublishThread(threading.Thread):
         if rospy.is_shutdown():
             raise Exception("Got shutdown request before subscribers connected")
 
-    def update(self, x, y, z, th, speed, turn):
+    def update(self, x, y, z, rx, ry, rz, lin_offset, ang_offset):
         self.condition.acquire()
-        self.x = x
-        self.y = y
-        self.z = z
-        self.th = th
-        self.speed = speed
-        self.turn = turn
+        self.linear_offset = lin_offset
+        self.angular_offset = ang_offset
+
+        # define increments given key inputs
+        self.x += x*lin_offset
+        self.y += y*lin_offset
+        self.z += z*lin_offset
+        
+        self.Q = trans.quaternion_multiply(self.Q, 
+                                           trans.quaternion_from_euler(rx*ang_offset, ry*ang_offset, rz*ang_offset))
+
+        #
+
         # Notify publish thread that we have a new message.
+
+        
+
         self.condition.notify()
         self.condition.release()
 
     def stop(self):
         self.done = True
-        self.update(0, 0, 0, 0, 0, 0)
+        # no need to send any command
+        # self.update(0, 0, 0, 0, 0, 0)
         self.join()
 
     def run(self):
-        twist_msg = TwistMsg()
+        pose_msg = PoseMsg()
 
         if stamped:
-            twist = twist_msg.twist
-            twist_msg.header.stamp = rospy.Time.now()
-            twist_msg.header.frame_id = twist_frame
+            pose = pose_msg.pose
+            pose_msg.header.stamp = rospy.Time.now()
+            pose_msg.header.frame_id = pose_frame
         else:
-            twist = twist_msg
+            pose = pose_msg
         while not self.done:
             if stamped:
-                twist_msg.header.stamp = rospy.Time.now()
+                pose_msg.header.stamp = rospy.Time.now()
             self.condition.acquire()
             # Wait for a new message or timeout.
             self.condition.wait(self.timeout)
 
-            # Copy state into twist message.
-            twist.linear.x = self.x * self.speed
-            twist.linear.y = self.y * self.speed
-            twist.linear.z = self.z * self.speed
-            twist.angular.x = 0
-            twist.angular.y = 0
-            twist.angular.z = self.th * self.turn
-
+           
+            pose.position.x = self.x 
+            pose.position.y = self.y 
+            pose.position.z = self.z            
+            pose.orientation.w = self.Q[3]
+            pose.orientation.x = self.Q[0]
+            pose.orientation.y = self.Q[1]
+            pose.orientation.z = self.Q[2]
+            # 
             self.condition.release()
 
             # Publish.
-            self.publisher.publish(twist_msg)
-
-        # Publish stop message when thread exits.
-        twist.linear.x = 0
-        twist.linear.y = 0
-        twist.linear.z = 0
-        twist.angular.x = 0
-        twist.angular.y = 0
-        twist.angular.z = 0
-        self.publisher.publish(twist_msg)
+            self.publisher.publish(pose_msg)
 
 
 def getKey(settings, timeout):
@@ -192,70 +226,71 @@ def restoreTerminalSettings(old_settings):
         return
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
-def vels(speed, turn):
-    return "currently:\tspeed %s\tturn %s " % (speed,turn)
+def vels(lin_offset, ang_offset):
+    return "currently:\tlin_offset %s\tang_offset %s " % (lin_offset,ang_offset)
 
 if __name__=="__main__":
     settings = saveTerminalSettings()
 
     rospy.init_node('teleop_twist_keyboard')
 
-    speed = rospy.get_param("~speed", 0.5)
-    turn = rospy.get_param("~turn", 1.0)
-    speed_limit = rospy.get_param("~speed_limit", 1000)
-    turn_limit = rospy.get_param("~turn_limit", 1000)
+    lin_offset = rospy.get_param("~lin_offset", 0.1)
+    ang_offset = rospy.get_param("~ang_offset", 1.0)
     repeat = rospy.get_param("~repeat_rate", 0.0)
     key_timeout = rospy.get_param("~key_timeout", 0.5)
-    stamped = rospy.get_param("~stamped", False)
-    twist_frame = rospy.get_param("~frame_id", '')
+    stamped = rospy.get_param("~stamped", True)
+    pose_frame = rospy.get_param("~frame_id", '')
     if stamped:
-        TwistMsg = TwistStamped
+        PoseMsg = PoseStamped
 
     pub_thread = PublishThread(repeat)
 
     x = 0
     y = 0
     z = 0
-    th = 0
+    rx = 0
+    ry = 0
+    rz = 0
     status = 0
 
     try:
         pub_thread.wait_for_subscribers()
-        pub_thread.update(x, y, z, th, speed, turn)
+        pub_thread.update(x, y, z, rx ,ry, rz, lin_offset, ang_offset)
 
         print(msg)
-        print(vels(speed,turn))
+        print(vels(lin_offset,ang_offset))
         while(1):
             key = getKey(settings, key_timeout)
             if key in moveBindings.keys():
                 x = moveBindings[key][0]
                 y = moveBindings[key][1]
                 z = moveBindings[key][2]
-                th = moveBindings[key][3]
-            elif key in speedBindings.keys():
-                speed = min(speed_limit, speed * speedBindings[key][0])
-                turn = min(turn_limit, turn * speedBindings[key][1])
-                if speed == speed_limit:
-                    print("Linear speed limit reached!")
-                if turn == turn_limit:
-                    print("Angular speed limit reached!")
-                print(vels(speed,turn))
+                rx = moveBindings[key][3]
+                ry = moveBindings[key][4]
+                rz = moveBindings[key][5]
+            elif key in offsetBindings.keys():
+                lin_offset = pub_thread.linear_offset*offsetBindings[key][0]
+                ang_offset = pub_thread.angular_offset*offsetBindings[key][1]
                 if (status == 14):
                     print(msg)
                 status = (status + 1) % 15
             else:
                 # Skip updating cmd_vel if key timeout and robot already
                 # stopped.
-                if key == '' and x == 0 and y == 0 and z == 0 and th == 0:
+                if key == '' and x == 0 and y == 0 and z == 0 and rx == 0 and ry == 0 and rz == 0:
                     continue
                 x = 0
                 y = 0
                 z = 0
-                th = 0
-                if (key == '\x03'):
-                    break
+                rx = 0
+                ry = 0
+                rz = 0
+            if (key == '\x03'):
+                pub_thread.stop()
+                break
+            
 
-            pub_thread.update(x, y, z, th, speed, turn)
+            pub_thread.update(x, y, z, rx ,ry, rz, lin_offset, ang_offset)
 
     except Exception as e:
         print(e)
